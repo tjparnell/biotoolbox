2998095b8cf63ddaeba11f2fcc6a871acaf52252
parnell.tj@gmail.com <parnell.tj@gmail.com@71660aa5-42f1-ed8c-46b2-3307acfe085d>
2012-07-17 16:14:18 +0000

    updated script verify_nucleosome_mapping.pl to accept any text file with coordinates, along with user provided database and datasets

--- scripts/verify_nucleosome_mapping.pl
+++ scripts/verify_nucleosome_mapping.pl
@@ -9,8 +9,12 @@ use Pod::Usage;
 use Statistics::Lite qw(min max statsinfo);
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
+use tim_data_helper qw(
+	find_column_index
+);
 use tim_db_helper qw(
 	open_db_connection
+	process_and_verify_dataset
 	get_region_dataset_hash
 );
 use tim_file_helper qw(
@@ -36,13 +40,21 @@ unless (@ARGV) {
 ### Get command line options and initialize values
 my (
 	$infile,
+	$database,
+	$dataset,
+	$outfile,
+	$gz,
 	$help,
 	$print_version,
 );
 
 # Command line options
 GetOptions( 
-	'in=s'      => \$infile, # the solexa data file
+	'in=s'      => \$infile, # the input data file
+	'db=s'      => \$database, # the database
+	'data=s'    => \$dataset, # the dataset to verify
+	'out=s'     => \$outfile, # the output file name
+	'gz!'       => \$gz, # compress output
 	'help'      => \$help, # request help
 	'version'   => \$print_version, # print the version
 ) or die " unrecognized option(s)!! please refer to the help documentation\n\n";
@@ -77,7 +89,7 @@ unless ($infile) {
 my $data_ref = load_tim_data_file($infile) or die 
 	" can't load file '$infile'!\n";
 unless ($data_ref->{feature} eq 'nucleosome') {
-	die " file feature is not 'nucleosome'! Are you loading the right file?\n";
+	warn " file feature is not 'nucleosome'! proceeding anyway\n";
 }
 print " Loaded file '$infile' with " . $data_ref->{last_row} . " nucleosomes\n";
 my $table = $data_ref->{data_table};
@@ -86,30 +98,39 @@ my $table = $data_ref->{data_table};
 
 ### Open db connection
 my $db;
-if ($data_ref->{'db'}) {
-	# a database was defined, so we'll use that
+if ($database) {
+	# a specific database was defined on the command line
+	$db = open_db_connection($database) or 
+		die " can't connect to database $database\n";
+}
+elsif ($data_ref->{'db'}) {
+	# a database was defined in the metadata
 	$db = open_db_connection($data_ref->{db}) or 
 		die " can't connect to database ". $data_ref->{db} . "\n";
 }
 else {
 	# no database was defined
 	# use one of the datasets
-	if (exists $data_ref->{5}{'dataset'}) {
-		# dataset in the Occupancy column
-		my $database = $data_ref->{5}{'dataset'};
-		$database =~ s/^\w+://;
-		$db = open_db_connection($database) or 
-			die " can't connect to database $database!\n";
-	}
-	elsif (exists $data_ref->{4}{'scan_dataset'} or exists $data_ref->{4}{'dataset'}) {
-		# dataset in the NucleosomeID column
-		my $database = $data_ref->{4}{'scan_dataset'} || $data_ref->{4}{'dataset'};
-		$database =~ s/^\w+://;
-		$db = open_db_connection($database) or 
-			die " can't connect to database $database!\n";
+	if ($dataset) {
+		$db = open_db_connection($dataset) or 
+			die " can't connect to a database!\n";
 	}
 	else {
-		die " no database defined in input file metadata!\n";
+		# look for a dataset defined in the metadata
+		my $index = find_column_index($data_ref, 'Occupancy') || 
+					find_column_index($data_ref, 'NucleosomeID') ||
+					find_column_index($data_ref, 'score') ||
+					undef;
+		unless (defined $index) {
+			die " Unable to identify Occupancy or Score dataset column!\n";
+		}
+		
+		# pull out the dataset name
+		$dataset = $data_ref->{$index}{'dataset'} || 
+				   $data_ref->{$index}{'scan_dataset'} || 
+				   undef; 
+		$db = open_db_connection($dataset) or 
+			die " can't connect to a database!\n";
 	}
 }
 
@@ -117,10 +138,34 @@ else {
 
 ### Define the dataset
 # defined in the Occupancy 
-my $dataset = $data_ref->{5}{'dataset'} ||      # Occupancy column
-			  $data_ref->{4}{'scan_dataset'} || # NucleosomeID column
-			  $data_ref->{4}{'dataset'} ||      # NucleosomeID column
-			  undef; 
+if ($dataset) {
+	# dataset defined on the command line
+	$dataset = process_and_verify_dataset( {
+		'db'        => $db,
+		'dataset'   => $dataset,
+		'prompt'    => "Enter the dataset to use for verifying nucleosome maps",
+		'single'    => 1,
+	} );
+}	
+else {	
+	# use a dataset defined in the input metadata
+	# usually defined in the Occupancy metadata, or maybe the NucleosomeID
+	# or maybe just the score column if nothing else is present
+	my $index = find_column_index($data_ref, 'Occupancy') || 
+				find_column_index($data_ref, 'NucleosomeID') ||
+				find_column_index($data_ref, 'score') ||
+				undef;
+	unless (defined $index) {
+		die " Unable to identify Occupancy or Score dataset column!\n";
+	}
+	
+	# pull out the dataset name
+	$dataset = $data_ref->{$index}{'dataset'} || 
+			   $data_ref->{$index}{'scan_dataset'} || 
+			   undef; 
+	# this should already have appropriate prefix if necessary
+	# hopefully we don't need to verify it
+}
 unless ($dataset) {
 	die " unable to identify the dataset in the input file metadata!\n";
 }
@@ -156,12 +201,14 @@ my @overlaps;
 my @peak_distances;
 my $on_target_count = 0;
 
-# identify indices
-# these are presumed from the map_nucleosomes.pl script
-my $chrom_i = 0;
-my $start_i = 1;
-my $stop_i = 2;
-my $mid_i = 3;
+# Identify indices
+my $chrom_i = find_column_index($data_ref, '^chrom|chr|seq|ref');
+my $start_i = find_column_index($data_ref, '^start');
+my $stop_i  = find_column_index($data_ref, '^stop|end');
+my $mid_i   = find_column_index($data_ref, '^midpoint|mid');
+unless (defined $chrom_i and defined $start_i and defined $stop_i) {
+	die " Unable to identify Chromosome, Start, and Stop columns!\n";
+}
 
 # main loop through data
 foreach (my $row = 1; $row <= $data_ref->{last_row}; $row++) {
@@ -192,14 +239,18 @@ foreach (my $row = 1; $row <= $data_ref->{last_row}; $row++) {
 	}
 	
 	## Check midpoint
+	# determine midpoint
+	my $midpoint = defined $mid_i ? $table->[$row][$mid_i] : 
+		int( ( ($table->[$row][$stop_i] + $table->[$row][$stop_i]) / 2) + 0.5 );
+	
 	# collect the raw data for 101 bp around the mapped midpoint
 	# this distance is arbitrary
 	my %nuc_data = get_region_dataset_hash( {
 		'db'         => $db,
 		'dataset'    => $dataset,
 		'chromo'     => $table->[$row][$chrom_i],
-		'start'      => $table->[$row][$mid_i] - 50,
-		'stop'       => $table->[$row][$mid_i] + 50,
+		'start'      => $midpoint - 50,
+		'stop'       => $midpoint + 50,
 		'value'      => 'score',
 		'absolute'   => 1,
 	});
@@ -213,7 +264,7 @@ foreach (my $row = 1; $row <= $data_ref->{last_row}; $row++) {
 			# store the distance between this peak and recorded midpoint
 			# these distances are usually positive because of the 
 			# way they are mapped - a premature call of a nucleosome
-			push @distances, $pos - $table->[$row][$mid_i];
+			push @distances, $pos - $midpoint;
 		}
 	}
 	# record the minimum distance from the recorded midpoint and occupancy peak
@@ -271,12 +322,19 @@ print "  of the remaining " . ($data_ref->{last_row} - $on_target_count) .
 
 
 ### Write results
+# check filename
+unless ($outfile) {
+	$outfile = $infile;
+}
+my $success;
 if (
-	write_tim_data_file( {
-		'data' => $data_ref,
+	$success = write_tim_data_file( {
+		'data'     => $data_ref,
+		'filename' => $outfile,
+		'gz'       => $gz,
 	}) 
 ) {
-	print " wrote data file\n";
+	print " wrote data file $success\n";
 }
 else {
 	print " unable to write file!\n";
@@ -297,6 +355,8 @@ verify_nucleosome_mapping.pl [--options...] <filename>
   
   Options:
   --in <filename>
+  --db <text>
+  --data <text | filename>
   --out <filename> 
   --(no)gz
   --version
@@ -311,9 +371,35 @@ The command line flags and descriptions:
 
 =item --in <filename>
 
-Specify the input file. The program expects the output data file from 
-the biotoolbox script 'map_nucleosomes.pl'. The file may be compressed 
-with gzip.
+Specify the input file. The program expects a text data file with 
+at least chromosome, start, and stop coordinates representing 
+nucleosome annotation. The output file from the biotoolbox script 
+'map_nucleosomes.pl' is ideal, although a Bed, GFF, or other file 
+is allowed. The file may be compressed with gzip.
+
+=item --db <text>
+
+Specify the name of a BioPerl database to pull the source data. A 
+SeqFeature::Store database or BigWigSet directory may be supplied. 
+The default is to use the database defined in the input file metadata 
+or the dataset file. 
+
+=item --data <text | filename>
+
+Provide the name of the dataset or data file (bigWig format)
+containing the nucleosome midpoint occupancy data with which to
+verify nucleosomal positions. If data is obtained from a database,
+the type or primary_tag should be provided. Default is to use the 
+dataset defined in the input file metadata.
+
+=item --out <filename>
+
+Optionally specify the output file name. By default it will overwrite 
+the input file.
+
+=item --gz
+
+Optionally indicate the file should be compressed when written.
 
 =item --version
 
@@ -327,24 +413,29 @@ Display this POD documentation.
 
 =head1 DESCRIPTION
 
-This program will verify the mapping of nucleosomes. It expects as input 
-the output data file from 'map_nucleosomes.pl'. Nucleosomes are verified 
-by comparing the peak of nucleosome reads collected from the original 
-dataset with the recorded midpoint of the mapped nucleosome. If the peak 
-is <= 10 bp from the recorded midpoint, then the nucleosome is considered 
-centered on the peak and it is properly mapped. If the peak is > 10 bp 
-from the recorded midpoint, then it is considered offset, or improperly 
-mapped. This is most often due to nucleosomes being called prematurely 
-when the dataset is being scanned in windows from left to right. 
-Adjusting the parameters for window and buffer in map_nucleosomes.pl can 
-limit the number of overlapping nucleosomes.
+This program will verify the mapping of nucleosomes. It expects as
+input a text file containing the genomic coordinates of annotated
+nucleosomes. The output data file from the BioToolBox script
+'map_nucleosomes.pl' is ideal, although other text files, including
+Bed and GFF, are supported. Nucleosomes are verified by comparing the
+peak of nucleosome reads collected from the original dataset with the
+recorded midpoint of the mapped nucleosome. If the peak is <= 10 bp
+from the recorded midpoint, then the nucleosome is considered centered
+on the peak and it is properly mapped. If the peak is > 10 bp from the
+recorded midpoint, then it is considered offset, or improperly mapped.
+This is most often due to nucleosomes being called prematurely when
+the dataset is being scanned in windows from left to right. Adjusting
+the parameters for window and buffer in map_nucleosomes.pl can limit
+the number of overlapping nucleosomes.
 
-The same data file is re-written with three additional columns appended, 
-overlap_length, center_peak_mapping, and center_peak_offset. 
+The same data file is re-written or a new file written with three 
+additional columns appended, overlap_length, center_peak_mapping, and 
+center_peak_offset. 
 
 Overlap_length records the amount of overlap between mapped nucleosomes. 
 Ideally this should be 0 as nucleosomes should not overlap; overlapping 
-nucleosomes indicate an error in mapping or multiple phasing of nucleosomes. 
+nucleosomes indicates either an error in mapping or multiple phasing of 
+nucleosomes. 
 
 Center_peak_mapping records whether the nucleosome was properly mapped or 
 not; one of two values is recorded: centered or offset.
