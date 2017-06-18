tjparnell/biotoolbox

commit c2d8927eaa3a6d24b8a260bfc256cdde720ca18b

fix and update statistics module requirements

lib/Bio/ToolBox/db_helper/useq.pm
@@ -4,7 +4,8 @@ package Bio::ToolBox::db_helper::useq;
 require Exporter;
 use strict;
 use Carp;
-use Statistics::Lite qw(mean);
+use List::Util qw(min max sum);
+use Statistics::Lite qw(median);
 use Bio::ToolBox::db_helper::constants;
 use Bio::DB::USeq;
 our $VERSION = '1.51';
@@ -230,7 +231,8 @@ sub collect_useq_position_scores {
 	}
 	elsif ($param->[METH] eq 'mean') {
 		foreach my $position (keys %pos2score) {
-			$pos2score{$position} = mean( @{$pos2score{$position}} );
+			$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
+									scalar( @{$pos2data{$position}} );
 		}
 	}
 	elsif ($param->[METH] eq 'median') {
@@ -256,7 +258,8 @@ sub collect_useq_position_scores {
 	else {
 		# just take the mean for everything else
 		foreach my $position (keys %pos2score) {
-			$pos2score{$position} = mean( @{$pos2score{$position}} );
+			$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
+									scalar( @{$pos2data{$position}} );
 		}
 	}
 	

lib/Bio/ToolBox/db_helper/bigbed.pm
@@ -4,7 +4,8 @@ package Bio::ToolBox::db_helper::bigbed;
 require Exporter;
 use strict;
 use Carp;
-use Statistics::Lite qw(mean);
+use List::Util qw(min max sum);
+use Statistics::Lite qw(median);
 use Bio::ToolBox::db_helper::constants;
 use Bio::DB::BigBed;
 our $VERSION = '1.51';
@@ -215,7 +216,8 @@ sub collect_bigbed_position_scores {
 	}
 	elsif ($param->[METH] eq 'mean') {
 		foreach my $position (keys %pos2data) {
-			$pos2data{$position} = mean( @{$pos2data{$position}} );
+			$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
+									scalar( @{$pos2data{$position}} );
 		}
 	}
 	elsif ($param->[METH] eq 'median') {
@@ -241,7 +243,8 @@ sub collect_bigbed_position_scores {
 	else {
 		# just take the mean for everything else
 		foreach my $position (keys %pos2data) {
-			$pos2data{$position} = mean( @{$pos2data{$position}} );
+			$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
+									scalar( @{$pos2data{$position}} );
 		}
 	}
 	

lib/Bio/ToolBox/db_helper/bigwig.pm
@@ -4,7 +4,7 @@ package Bio::ToolBox::db_helper::bigwig;
 require Exporter;
 use strict;
 use Carp;
-use Statistics::Lite qw(min max mean);
+use List::Util qw(min max sum);
 use Bio::ToolBox::db_helper::constants;
 use Bio::DB::BigWig qw(binMean binStdev);
 use Bio::DB::BigWigSet;
@@ -471,7 +471,7 @@ sub _remove_duplicate_positions {
 			push @values, $pos2data->{ "$pos\.$i" };
 			delete $pos2data->{ "$pos\.$i" };
 		}
-		$pos2data->{$pos} = mean(@values);
+		$pos2data->{$pos} = sum(@values) / scalar(@values);
 	}
 }
 

lib/Bio/ToolBox/db_helper/seqfasta.pm
@@ -4,7 +4,8 @@ require Exporter;
 use Carp;
 use strict;
 use Module::Load; # for dynamic loading during runtime
-use Statistics::Lite qw(mean);
+use List::Util qw(min max sum);
+use Statistics::Lite qw(median);
 use Bio::ToolBox::db_helper::config;
 use Bio::ToolBox::db_helper::constants;
 use Bio::DB::Fasta;
@@ -324,7 +325,8 @@ sub collect_store_scores {
 		}
 		elsif ($param->[METH] eq 'mean') {
 			foreach my $position (keys %pos2data) {
-				$pos2data{$position} = mean( @{$pos2data{$position}} );
+				$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
+										scalar( @{$pos2data{$position}} );
 			}
 		}
 		elsif ($param->[METH] eq 'median') {
@@ -350,7 +352,8 @@ sub collect_store_scores {
 		else {
 			# just take the mean for everything else
 			foreach my $position (keys %pos2data) {
-				$pos2data{$position} = mean( @{$pos2data{$position}} );
+				$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
+										scalar( @{$pos2data{$position}} );
 			}
 		}
 	}

commit 650fdcf9337d7102e3c6fe832abd0d884e74e493

fix bug introduced in c2d8927 commit

lib/Bio/ToolBox/db_helper/useq.pm
@@ -231,8 +231,8 @@ sub collect_useq_position_scores {
 	}
 	elsif ($param->[METH] eq 'mean') {
 		foreach my $position (keys %pos2score) {
-			$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
-									scalar( @{$pos2data{$position}} );
+			$pos2score{$position} = sum( @{$pos2score{$position}} ) / 
+									scalar( @{$pos2score{$position}} );
 		}
 	}
 	elsif ($param->[METH] eq 'median') {
@@ -258,8 +258,8 @@ sub collect_useq_position_scores {
 	else {
 		# just take the mean for everything else
 		foreach my $position (keys %pos2score) {
-			$pos2data{$position} = sum( @{$pos2data{$position}} ) / 
-									scalar( @{$pos2data{$position}} );
+			$pos2score{$position} = sum( @{$pos2score{$position}} ) / 
+									scalar( @{$pos2score{$position}} );
 		}
 	}
 	