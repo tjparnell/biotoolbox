287f6b36b870d53bc448f897797552131e187b49
Timothy Parnell <parnell.tj@gmail.com>
2019-09-11 21:51:21 -0600

    fix again the print statements when removing specific values

--- scripts/manipulate_datasets.pl
+++ scripts/manipulate_datasets.pl
@@ -1330,18 +1330,19 @@ sub do_specific_values_function {
 	
 	# report
 	if ($toss) {
-		printf " %s rows with specific values of %s in columns %s were deleted.\n", 
+		printf " %s rows with specific values of %s were deleted in columns %s.\n", 
 			format_with_commas(scalar(@todelete)), join(', ', keys %wanted),
 			join(', ', map {$Data->name($_)} @list),
+		printf " %s data lines are remaining\n", format_with_commas($Data->last_row);
 	}
 	else {
-		printf " %s rows with specific values of %s in columns %s were retained.\n", 
+		printf " %s rows were deleted in columns %s\n",
+			format_with_commas(scalar(@todelete)),
+			join(', ', map {$Data->name($_)} @list);
+		printf " %s rows with specific values of %s were retained.\n",
 			format_with_commas( $start_number - scalar(@todelete) ), 
-			join(', ', keys %wanted),
-			join(', ', map {$Data->name($_)} @list),
-		printf " %s rows were deleted", format_with_commas(scalar(@todelete));
+			join(', ', keys %wanted);
 	}
-	printf " %s data lines are remaining\n", format_with_commas($Data->last_row);
 	return 1;
 }
 
